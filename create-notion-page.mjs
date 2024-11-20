19d973477c470127fda01c217e0130a026c59b28
moonhr <moonhr153@gmail.com>
2024-11-20 12:06:51 +0900

    database url

--- create-notion-page.mjs
+++ create-notion-page.mjs
@@ -3,7 +3,7 @@ import fetch from "node-fetch"; // `require` 대신 `import` 사용
 
 async function createNotionPage() {
   const notionApiUrl = "https://api.notion.com/v1/pages";
-  const databaseId = "14492ced8e5f8057a6c0fe416302138d"; // Notion 데이터베이스 ID를 여기에 입력하세요
+  const databaseId = "14492ced8e5f800b979ac1d7e55e25ca"; // Notion 데이터베이스 ID를 여기에 입력하세요
   const notionToken = process.env.NOTION_API_TOKEN;
 
   const response = await fetch(notionApiUrl, {
